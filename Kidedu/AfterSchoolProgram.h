ad4bde8d6a5fec3b77e1d8264b2a9178c99f9c43
Tamar Kandathi <tamar.kandathi@gmail.com>
2016-04-22 13:39:25 -0400

    AfterSchoolProgramClass

---
+++ Kidedu/AfterSchoolProgram.h
@@ -0,0 +1,18 @@
+//
+//  AfterSchoolProgram.h
+//  Kidedu
+//
+//  Created by Tamar on 4/22/16.
+//  Copyright © 2016 Tamar Kandathi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AfterSchoolProgram : NSObject
+
+@property (nonatomic, strong) NSString *borough;
+@property (nonatomic, strong) NSString *location;
+@property (nonatomic, strong) NSString *programName;
+@property (nonatomic, strong) NSString *contactNumber;
+@property (nonatomic, strong) NSString *gradeLevel;
+@end
---
+++ Kidedu/AfterSchoolProgram.m
@@ -0,0 +1,13 @@
+//
+//  AfterSchoolProgram.m
+//  Kidedu
+//
+//  Created by Tamar on 4/22/16.
+//  Copyright © 2016 Tamar Kandathi. All rights reserved.
+//
+
+#import "AfterSchoolProgram.h"
+
+@implementation AfterSchoolProgram
+
+@end
