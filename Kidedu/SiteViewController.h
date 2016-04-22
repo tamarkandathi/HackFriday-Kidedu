7f1f7f1497381c4be72fecb5a70c3c562c842f5f
Benjamin Soung <soungbenjamin@gmail.com>
2016-04-22 14:32:06 -0400

    added new view controller

---
+++ Kidedu/SiteViewController.h
@@ -0,0 +1,13 @@
+//
+//  SiteViewController.h
+//  Kidedu
+//
+//  Created by Benjamin Soung on 4/22/16.
+//  Copyright © 2016 Tamar Kandathi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SiteViewController : UIViewController
+
+@end
